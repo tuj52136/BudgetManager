ebc89282f6695d1d2a91d0f3fba0e0c381510efb
Leo Vergnetti <tuj52136@cst11513.tu.temple.edu>
2019-04-11 11:22:14 -0400

    Initial Commit

---
+++ BudgetManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BudgetManager
+//
+//  Created by Leo Vergnetti on 4/11/19.
+//  Copyright © 2019 Leo Vergnetti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ BudgetManagerTests/BudgetManagerTests.swift
@@ -0,0 +1,34 @@
+//
+//  BudgetManagerTests.swift
+//  BudgetManagerTests
+//
+//  Created by Leo Vergnetti on 4/11/19.
+//  Copyright © 2019 Leo Vergnetti. All rights reserved.
+//
+
+import XCTest
+@testable import BudgetManager
+
+class BudgetManagerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
